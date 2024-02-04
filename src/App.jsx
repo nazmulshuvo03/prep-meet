b3119526f2d394b6221f504da20f57d843abcae2
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-04 23:39:51 +0600

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import Profile from "./routes/Profile";
 
 function App() {
   const global = useSelector((state) => state.global);
-  const profile = useSelector((state) => state.user.profile);
 
   return (
     <div
@@ -17,24 +16,22 @@ function App() {
       } h-screen w-screen overflow-hidden`}
     >
       <div className={`bg-background text-text h-full w-full overflow-y-auto`}>
-        {profile && (
-          <BrowserRouter basename="/">
-            <div className="fixed top-0 left-0 w-full h-24">
-              <Navigation />
-            </div>
-            <div
-              className="mt-24 p-4"
-              style={{ height: "-webkit-fill-available" }}
-            >
-              <Switch>
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/dashboard/:userId" component={Home} />
-                <Route exact path="/account" component={Account} />
-                <Route exact path="/profile/:userId" component={Profile} />
-              </Switch>
-            </div>
-          </BrowserRouter>
-        )}
+        <BrowserRouter basename="/">
+          <div className="fixed top-0 left-0 w-full h-24">
+            <Navigation />
+          </div>
+          <div
+            className="mt-24 p-4"
+            style={{ height: "-webkit-fill-available" }}
+          >
+            <Switch>
+              <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/dashboard/:userId" component={Home} />
+              <Route exact path="/account" component={Account} />
+              <Route exact path="/profile/:userId" component={Profile} />
+            </Switch>
+          </div>
+        </BrowserRouter>
       </div>
     </div>
   );
