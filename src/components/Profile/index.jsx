5fb9f4b50a588576f35db044c5941e5acfd81f7f
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-27 20:43:07 +0600

    old pages brought to new design

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,7 +1,21 @@
+import { useSelector } from "react-redux";
+import { AvailableTimes } from "../../components/AvailableTimes";
+import { MyMeetings } from "../../components/MyMeetings";
+import { PersonalAccount } from "../../components/PersonalAccount";
+import { Tabs } from "../../components/Tabs";
+
 export const Profile = () => {
+  const profile = useSelector((state) => state.user.profile);
+
+  const TABS = [
+    { id: 1, name: "Personal Details", component: <PersonalAccount /> },
+    { id: 2, name: "Available Times", component: <AvailableTimes /> },
+    { id: 3, name: "My Meetings", component: <MyMeetings /> },
+  ];
+
   return (
-    <div>
-      <h1>This is profile page</h1>
+    <div className="flex items-center justify-center w-full h-full overflow-hidden">
+      {profile && <Tabs data={TABS} />}
     </div>
   );
 };
