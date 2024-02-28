482e6a47269ad0f92589e1f589e49298b196182c
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-28 19:36:17 +0600

    people comopnent updated

--- src/components/People/AllPeople.jsx
+++ src/components/People/AllPeople.jsx
@@ -1,79 +1,27 @@
-import { useEffect, useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import LANGUAGE_DATA from "../../assets/data/languages.json";
 import COUNTRY_DATA from "../../assets/data/countries.json";
-
-import { fetchPeople } from "../../store/middlewares/user";
 import { PersonCard } from "../../components/Cards/PersonCard";
 import { Button } from "../../components/Button";
 import { Dropdown } from "../../components/Dropdown";
 import { RangeSlider } from "../../components/Slider/RangeSlider";
-import { setDashboardQuery } from "../../store/slices/global";
-import { queryObjectToString } from "../../utils/query";
-import { deepEqual, isEmptyObject } from "../../utils/object";
 
 const EXPERIENCE_MIN_VALUE = 0;
 const EXPERIENCE_MAX_VALUE = 20;
 
-export const AllPeople = () => {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const profile = useSelector((state) => state.user.profile);
+export const AllPeople = ({
+  queries = {},
+  handleQuerySelect = () => {},
+  handleResetQuery = () => {},
+  minExp = EXPERIENCE_MIN_VALUE,
+  maxExp = EXPERIENCE_MAX_VALUE,
+  handleExperienceSelect = () => {},
+}) => {
   const people = useSelector((state) => state.user.people);
-  const oldQuery = useSelector((state) => state.global.dashboardQuery);
   const professions = useSelector((state) => state.profession.items);
 
-  const [queries, setQueries] = useState({});
-  const [minExp, setMinExp] = useState(EXPERIENCE_MIN_VALUE);
-  const [maxExp, setMaxExp] = useState(EXPERIENCE_MAX_VALUE);
-
-  useEffect(() => {
-    if (oldQuery) {
-      setQueries(oldQuery);
-      setMaxExp(oldQuery["yearsOfExperience_lte"]);
-      setMinExp(oldQuery["yearsOfExperience_gte"]);
-    }
-  }, [oldQuery]);
-
-  useEffect(() => {
-    if (queries) {
-      if (!isEmptyObject(queries) && !deepEqual(queries, oldQuery)) {
-        dispatch(setDashboardQuery(queries));
-      }
-      const queryString = queryObjectToString(queries);
-      if (profile) {
-        dispatch(fetchPeople(profile.id, queryString));
-        history.push(`/dashboard?${queryString}`);
-      }
-    }
-  }, [queries, oldQuery, profile]);
-
-  const handleQuerySelect = (e) => {
-    const { name, value } = e.target;
-    setQueries((prevParams) => ({
-      ...prevParams,
-      [name]: value,
-    }));
-  };
-
-  const handleExperienceSelect = (values, i) => {
-    setMaxExp(values[1]);
-    setMinExp(values[0]);
-    setQueries((prev) => ({
-      ...prev,
-      yearsOfExperience_lte: values[1],
-      yearsOfExperience_gte: values[0],
-    }));
-  };
-
-  const handleResetQuery = () => {
-    setQueries({});
-    setMinExp(EXPERIENCE_MIN_VALUE);
-    setMaxExp(EXPERIENCE_MAX_VALUE);
-    dispatch(setDashboardQuery(""));
-  };
+  console.log("@@@@@@@@", typeof people);
 
   return (
     <div className="py-2">
@@ -116,7 +64,7 @@ export const AllPeople = () => {
         />
       </div>
       <div className="grid grid-cols-3 gap-4">
-        {people && people.length
+        {people && people.length && typeof people !== "string"
           ? people.map((person) => (
               <Link
                 key={person.id}
--- src/components/People/index.jsx
+++ src/components/People/index.jsx
@@ -1,13 +1,89 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPeople } from "../../store/middlewares/user";
+import { setDashboardQuery } from "../../store/slices/global";
+import { queryObjectToString } from "../../utils/query";
+import { deepEqual, isEmptyObject } from "../../utils/object";
 import { HorizontalTabs } from "../Tabs/HorizontalTabs";
 import { AllPeople } from "./AllPeople";
 import { FavouritePeople } from "./FavouritePeople";
 
+const EXPERIENCE_MIN_VALUE = 0;
+const EXPERIENCE_MAX_VALUE = 20;
+
 export const People = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.user.profile);
+  const oldQuery = useSelector((state) => state.global.dashboardQuery);
+
+  const [queries, setQueries] = useState({});
+  const [minExp, setMinExp] = useState(EXPERIENCE_MIN_VALUE);
+  const [maxExp, setMaxExp] = useState(EXPERIENCE_MAX_VALUE);
+
+  useEffect(() => {
+    if (oldQuery) {
+      setQueries(oldQuery);
+      setMaxExp(oldQuery["yearsOfExperience_lte"]);
+      setMinExp(oldQuery["yearsOfExperience_gte"]);
+    }
+  }, [oldQuery]);
+
+  useEffect(() => {
+    if (queries) {
+      if (!isEmptyObject(queries) && !deepEqual(queries, oldQuery)) {
+        dispatch(setDashboardQuery(queries));
+      }
+      const queryString = queryObjectToString(queries);
+      if (profile) {
+        dispatch(fetchPeople(profile.id, queryString));
+        history.push(`/dashboard?${queryString}`);
+      }
+    }
+  }, [queries, oldQuery, profile]);
+
+  const handleQuerySelect = (e) => {
+    const { name, value } = e.target;
+    setQueries((prevParams) => ({
+      ...prevParams,
+      [name]: value,
+    }));
+  };
+
+  const handleExperienceSelect = (values, i) => {
+    setMaxExp(values[1]);
+    setMinExp(values[0]);
+    setQueries((prev) => ({
+      ...prev,
+      yearsOfExperience_lte: values[1],
+      yearsOfExperience_gte: values[0],
+    }));
+  };
+
+  const handleResetQuery = () => {
+    setQueries({});
+    setMinExp(EXPERIENCE_MIN_VALUE);
+    setMaxExp(EXPERIENCE_MAX_VALUE);
+    dispatch(setDashboardQuery(""));
+  };
+
   const TABS = [
     {
       id: 1,
       name: "All",
-      component: <AllPeople />,
+      component: (
+        <AllPeople
+          {...{
+            queries,
+            handleQuerySelect,
+            handleResetQuery,
+            minExp,
+            maxExp,
+            handleExperienceSelect,
+          }}
+        />
+      ),
     },
     {
       id: 2,
