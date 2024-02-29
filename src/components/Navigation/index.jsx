b5e6cebba1d32454df0334729c8bff3256434988
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-02-29 20:26:19 +0600

    under constuction banner added

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,6 +2,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useLocation, useHistory } from "react-router-dom";
 import { Button } from "../Button";
 import { logoutUser } from "../../store/middlewares/auth";
+import { Banner } from "../Banner";
 
 export const Navigation = () => {
   // const location = useLocation();
@@ -29,34 +30,41 @@ export const Navigation = () => {
   };
 
   return (
-    <div className="bg-primary flex justify-between w-full h-full items-center px-5">
-      <div className="text-3xl font-semibold opacity-75 ">
-        <NavLink to={"/dashboard"} className="text-gray-900">
-          Candidace
-        </NavLink>
-      </div>
-      <div className="flex gap-8 items-center">
-        <nav className="flex items-center gap-8">
-          {navLinks.map((link) => (
-            <NavLink
-              key={link.name}
-              to={link.to}
-              // isActive={() => isRouteActive(link.to)}
-              className="h-fit rounded-lg text-slate-950 dark:text-slate-50"
-              // activeClassName="bg-secondary !text-slate-50 dark:!text-slate-100 font-semibold"
-            >
-              {link.name}
-            </NavLink>
-          ))}
-        </nav>
-        <Button
-          className={
-            "border border-gray-700 !bg-white rounded-none !font-light !text-gray-700"
-          }
-          onClick={handleLogout}
-        >
-          Sign Out
-        </Button>
+    <div>
+      <Banner
+        text={
+          "This website is currently under active construction. Please inform us of any anomalies or issues you encounter. Thank you for your patience."
+        }
+      />
+      <div className="bg-primary flex justify-between w-full h-full items-center px-5">
+        <div className="text-3xl font-semibold opacity-75 ">
+          <NavLink to={"/dashboard"} className="text-gray-900">
+            Candidace
+          </NavLink>
+        </div>
+        <div className="flex gap-8 items-center">
+          <nav className="flex items-center gap-8">
+            {navLinks.map((link) => (
+              <NavLink
+                key={link.name}
+                to={link.to}
+                // isActive={() => isRouteActive(link.to)}
+                className="h-fit rounded-lg text-slate-950 dark:text-slate-50"
+                // activeClassName="bg-secondary !text-slate-50 dark:!text-slate-100 font-semibold"
+              >
+                {link.name}
+              </NavLink>
+            ))}
+          </nav>
+          <Button
+            className={
+              "border border-gray-700 !bg-white rounded-none !font-light !text-gray-700"
+            }
+            onClick={handleLogout}
+          >
+            Sign Out
+          </Button>
+        </div>
       </div>
     </div>
   );
