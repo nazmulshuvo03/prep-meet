6e24c14240d837a21c3e5d57c69b8ff3070262d7
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-13 00:16:54 +0600

    profile design updated

--- src/components/Profile/Info/Target.jsx
+++ src/components/Profile/Info/Target.jsx
@@ -24,11 +24,11 @@ export const Target = () => {
   const [editMode, setEditMode] = useState(false);
 
   return (
-    <div className="relative col-span-8 flex flex-col gap-2">
+    <div className="relative col-span-8 flex flex-col gap-2 pt-4">
       <div className="flex flex-col gap-2 mb-4">
         <div className="flex flex-wrap items-baseline gap-1 break-words">
-          <div className="text-sm font-semibold">Target Company:</div>
-          <div className="flex gap-1 flex-wrap break-words text-xs font-semibold">
+          <div className="text-base font-semibold">Target Company:</div>
+          <div className="flex gap-1 flex-wrap break-words text-base font-semibold">
             {profile.companiesOfInterest &&
               profile.companiesOfInterest.length &&
               profile.companiesOfInterest.map((comp, i) => {
@@ -48,6 +48,15 @@ export const Target = () => {
                   </div>
                 );
               })}
+          </div>
+        </div>
+
+        <div className="flex gap-1 items-baseline">
+          <div className="text-xs font-semibold">Target Role:</div>
+          <div className="text-xs font-medium">
+            {profile && profile.targetProfession
+              ? profile.targetProfession.name || ""
+              : ""}
           </div>
         </div>
 
@@ -75,15 +84,15 @@ export const Target = () => {
           </div>
         </div>
 
-        <div className="flex flex-wrap items-baseline gap-1 break-words">
-          <div className="text-xs font-semibold">Focus Areas:</div>
+        <div className="flex flex-wrap items-baseline gap-1 break-words py-1">
+          <div className="text-xs font-semibold">Practice Areas:</div>
           <div className="flex gap-1 flex-wrap break-words text-xs font-normal">
             {profile.focusAreas &&
               profile.focusAreas.length &&
               profile.focusAreas.map((focus, i) => {
                 return (
                   <div key={focus}>
-                    <span className="bg-purple-100 text-purple-800 px-4 py-0.5 rounded-full">
+                    <span className="bg-purple-100 text-purple-800 px-4 py-0 rounded-full">
                       {getDataLabelFromKey(allSkill, focus)}
                     </span>
                   </div>
@@ -92,7 +101,7 @@ export const Target = () => {
           </div>
         </div>
 
-        <div className="flex flex-wrap items-baseline gap-1 break-words">
+        <div className="flex flex-wrap items-baseline gap-1 break-words py-1">
           <div className="text-xs font-semibold">Experience Types:</div>
           <div className="flex gap-1 flex-wrap break-words text-xs font-normal">
             {profile.typesOfExperience &&
@@ -100,7 +109,7 @@ export const Target = () => {
               profile.typesOfExperience.map((types, i) => {
                 return (
                   <div key={types}>
-                    <span className="bg-blue-100 text-blue-800 px-4 py-0.5 rounded-full">
+                    <span className="bg-blue-100 text-blue-800 px-4 py-0 rounded-full">
                       {getDataLabelFromKey(allExperienceType, types)}
                     </span>
                   </div>
@@ -111,7 +120,7 @@ export const Target = () => {
       </div>
       {profile.profileHeadline && profile.profileHeadline.length && (
         <div className="h-full">
-          <div className="text-sm font-normal text-gray-600 border border-gray-200 p-2 h-full">
+          <div className="text-xs font-normal text-text py-2 h-full">
             {profile.profileHeadline}
           </div>
         </div>
--- src/components/Profile/Info/index.jsx
+++ src/components/Profile/Info/index.jsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faBriefcase } from "@fortawesome/free-solid-svg-icons";
 import { UserName } from "./UserName";
 import { Target } from "./Target";
+import { AdditionalInfo } from "../../PersonCard/AdditionalInfo";
 
 export const Info = () => {
   const profile = useSelector((state) => state.user.profile);
@@ -11,59 +12,46 @@ export const Info = () => {
   );
 
   return (
-    <div className="bg-white p-5 h-full w-full">
-      <div className="grid grid-cols-12 gap-5">
-        <div className="col-span-4 px-5">
-          <div className="flex flex-col items-center">
-            <img
-              src={profile.photoURL}
-              alt={"Person Profile Image"}
-              className="h-32 w-32 rounded-md"
-            />
-            <div className="flex gap-2 font-semibold text-md">
-              <span>{profile.firstName}</span>
-              <span>{profile.lastName}</span>
-            </div>
-            <div className="text-sm font-light text-gray-500">
-              {profile.country}
-            </div>
-          </div>
-          <div className="pt-5">
-            {profile.email ? (
-              <div className="flex gap-2 items-center">
-                <FontAwesomeIcon
-                  className="text-xs text-gray-500"
-                  icon={faEnvelope}
+    <div className="bg-white p-2 h-full w-full flex flex-col">
+      <div className="flex-1 p-2">
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-span-4 px-4">
+            <div className="flex items-center justify-start">
+              <div className="flex flex-col items-center">
+                <img
+                  src={profile.photoURL}
+                  alt={"Person Profile Image"}
+                  className="h-32 w-32 rounded-md my-2"
                 />
-                <div className="text-md text-gray-500">{profile.email}</div>
-              </div>
-            ) : (
-              ""
-            )}
-            {targetProfession && targetProfession.name ? (
-              <div className="flex gap-2 items-center">
-                <FontAwesomeIcon
-                  className="text-xs text-gray-500"
-                  icon={faBriefcase}
-                />
-                <div className="text-md text-gray-500">
-                  {targetProfession.name}
+                <div className="flex gap-2 font-semibold text-lg">
+                  <span>{profile.firstName}</span>
+                  <span>{profile.lastName}</span>
+                </div>
+                <div className="text-sm font-normal text-gray-500">
+                  {profile.country}
                 </div>
               </div>
-            ) : (
-              ""
-            )}
-            <UserName />
+            </div>
+            <div className="pt-5">
+              {profile.email ? (
+                <div className="flex gap-2 items-center">
+                  <FontAwesomeIcon
+                    className="text-xs text-gray-500"
+                    icon={faEnvelope}
+                  />
+                  <div className="text-md text-gray-500">{profile.email}</div>
+                </div>
+              ) : (
+                ""
+              )}
+              <UserName />
+            </div>
           </div>
+          <Target />
         </div>
-        <Target />
       </div>
-      <div className="flex gap-2 justify-end text-xs font-bold mt-4">
-        <div>cancelation: N/A</div>
-        <div>depth of feedback: N/A</div>
-        <div>product sense: N/A</div>
-        <div>analytics and metrics: N/A</div>
-        <div>behavioral: N/A</div>
+      <div className="flex justify-end items-center mt-4">
+        <AdditionalInfo />
       </div>
     </div>
   );
