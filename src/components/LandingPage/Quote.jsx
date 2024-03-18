34b2c512d14db46a3a946e3ee4a638d23d147cdc
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-19 00:41:43 +0600

    quote section added

--- src/components/LandingPage/Quote.jsx
+++ src/components/LandingPage/Quote.jsx
@@ -1,7 +1,11 @@
 export const Quote = () => {
   return (
-    <div>
-      <h1>Quote</h1>
+    <div className="px-12 md:px-28 py-16">
+      <div className="text-4xl font-bold text-center md:px-24">
+        {`“Practicing mock interviews through Candidace.fyi was a game changer.
+        The insiders’ knowledge helped me understand what to really expect.”`}
+      </div>
+      <div className="text-end">-Hiring Success Story</div>
     </div>
   );
 };
