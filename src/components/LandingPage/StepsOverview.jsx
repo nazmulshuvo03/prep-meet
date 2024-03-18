1d379fdfc65a32044d3c23050ac999d3a97482b3
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-19 00:33:46 +0600

    steps overview section created

--- src/components/LandingPage/StepsOverview.jsx
+++ src/components/LandingPage/StepsOverview.jsx
@@ -1,7 +1,41 @@
 export const StepsOverview = () => {
   return (
-    <div>
-      <h1>StepsOverview</h1>
+    <div className="flex flex-col-reverse md:flex-row items-center px-10 py-14 bg-white">
+      <div className="flex-1 flex flex-col items-start justify-start gap-6">
+        <div>
+          <div className="text-3xl font-bold py-1">
+            Sign Up and Connect with Peers
+          </div>
+          <div className="text-base font-light py-1">
+            Create an account and start connecting with professionals in your
+            field. Book practice sessions around your schedule.
+          </div>
+        </div>
+        <div>
+          <div className="text-3xl font-bold py-1">
+            Schedule and Practice Interviews
+          </div>
+          <div className="text-base font-light py-1">
+            Take turns being the interviewee and the interviewer. Get
+            perspectives from diverse candidates.
+          </div>
+        </div>
+        <div>
+          <div className="text-3xl font-bold py-1">
+            Get Honest, Valuable Feedback
+          </div>
+          <div className="text-base font-light py-1">
+            Get specific feedback to improve your skills. Track your progress
+            over time and identify growth areas.
+          </div>
+        </div>
+      </div>
+      <div>
+        <img
+          src="/images/landingPage/image3.png"
+          alt="Landing Your Dream Job"
+        />
+      </div>
     </div>
   );
 };
