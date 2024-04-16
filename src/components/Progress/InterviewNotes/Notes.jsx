f9dfbc5e6d67170160044fda631ec71d35e845db
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-04-16 16:44:14 +0600

    interview notes interviewer proifle added

--- src/components/Progress/InterviewNotes/Notes.jsx
+++ src/components/Progress/InterviewNotes/Notes.jsx
@@ -1,15 +1,46 @@
+import { useSelector } from "react-redux";
+import { convertISOUTCDayTimeToLocalDayTime } from "../../../utils/timeDate";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 export const Notes = ({ data = null }) => {
+  const user = useSelector((state) => state.user.profile);
+
   return (
     <div>
       {data && data.length ? (
         <>
           {data.map((item) => {
+            const interviewerProfile =
+              user.id === item.meeting.acceptorProfile.id
+                ? item.meeting.initiatorProfile
+                : item.meeting.acceptorProfile;
             return (
               <div
                 key={item.id}
-                className="flex my-3 bg-background py-2 px-1 rounded-sm text-gray-600"
+                className="my-3 bg-background py-2 px-1 rounded-sm text-gray-600"
               >
-                {item.note}
+                <div className="pt-1 pb-3">{item.note}</div>
+                <Link
+                  to={`/user/${interviewerProfile.id}`}
+                  className="flex gap-1 text-gray-500 text-xs italic"
+                >
+                  <div>-</div>
+                  <div>{interviewerProfile.userName},</div>
+                  <div>
+                    {
+                      convertISOUTCDayTimeToLocalDayTime(
+                        item.meeting.dayHourUTC
+                      ).dateMonthView
+                    }
+                  </div>
+                  <div>
+                    {
+                      convertISOUTCDayTimeToLocalDayTime(
+                        item.meeting.dayHourUTC
+                      ).time
+                    }
+                  </div>
+                </Link>
               </div>
             );
           })}
