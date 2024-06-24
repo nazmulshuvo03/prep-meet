b876a669525111141f40c4f759ae87f3c90e1cac
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-06-25 00:11:08 +0600

    notification bell updated

--- src/components/Notification/Bell.jsx
+++ src/components/Notification/Bell.jsx
@@ -9,12 +9,16 @@ export const Bell = () => {
   return (
     <div className="relative">
       <FontAwesomeIcon icon={faBell} className="w-5 h-5" />
-      <div
-        className="absolute -top-1/2 -right-3 translate-y-1/4 bg-red-500 text-white w-4 h-4 flex justify-center items-center rounded-full"
-        style={{ fontSize: "8px" }}
-      >
-        {unreadCount}
-      </div>
+      {unreadCount ? (
+        <div
+          className="absolute -top-1/2 -right-3 translate-y-1/4 bg-red-500 text-white w-4 h-4 flex justify-center items-center rounded-full"
+          style={{ fontSize: "8px" }}
+        >
+          {unreadCount}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
